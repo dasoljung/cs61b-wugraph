aa88ff1eec6a5c85aec3318077429cd4163bf538
Dasol Jung <dasol.jung@gmail.com>
2012-04-14 18:54:27 -0700

    changed

--- src/Kruskal.java
+++ src/Kruskal.java
@@ -3,6 +3,11 @@
 import java.util.Hashtable;
 import graph.*;
 import set.*;
+import graph.*;
+import graph.*
+;
+import graph.*;
+import graph.Neighbors;
 
 /**
  * The Kruskal class contains the method minSpanTree(), which implements
@@ -15,6 +20,33 @@ public class Kruskal {
    * minSpanTree() returns a WUGraph that represents the minimum spanning tree
    * of the WUGraph g.  The original WUGraph g is NOT changed.
    */
-  public static WUGraph minSpanTree(WUGraph g){return null;};
+  public static WUGraph minSpanTree(WUGraph g){
+	  WUGraph t = new WUGraph();
+	  Object[] vertices = g.getVertices();
+	  Object[] edges = new Object[g.edgeCount()];
+	  for (Object o: vertices){
+		  t.addVertex(o);
+		  if (edges[-1] == null) {
+			  Neighbors n = g.getNeighbors(o);
+			  for (int i=0; i < n.neighborList.length; i++) {
+				  new EdgeWeight(o, n.neighborList[i], n.weightList[i]);
+				  
+			  }
+		  }
+	  }
+	  
+	  return t;
+  }
 
+}
+
+class EdgeWeight {
+	public Object v1;
+	public Object v2;
+	int weight;
+	public EdgeWeight(Object o1, Object o2, int weight) {
+		v1 = o1;
+		v2 = o2;
+		this.weight = weight;
+	}
 }
